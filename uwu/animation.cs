4d422cc726be214a11d11d26f1ad941673f4dd1d
Spencer Morga <Spencer.Morga@GMR.local>
2019-02-03 14:34:06 -0800

    initial commit

---
+++ uwu/animation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uwu
+{
+    public class animation : sprite
+    {
+        TimeSpan elaspedTime;
+        public TimeSpan waitingtime;
+        public List<Rectangle> frames;
+        public int currentframeIndex = 0;
+        
+            public animation(Texture2D image, Vector2 position, Color color, List<Rectangle> frames)
+                : base(image, position, color)
+            {
+                this.frames = frames;
+                waitingtime = TimeSpan.FromMilliseconds(90);
+            }
+            
+        public void Update (GameTime gTime)
+        {
+            elaspedTime += gTime.ElapsedGameTime;
+        }
+    }
+}
---
+++ uwu/sprite.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uwu
+{
+    public class sprite
+    {
+        Texture2D image;
+        Vector2 position;
+        Color color;
+        Rectangle sourceRectangle;
+
+        public sprite(Texture2D image, Vector2 position, Color color)
+        {
+            this.image = image;
+            this.position = position;
+            this.color = color;
+        }
+
+        public void Draw (SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(image, position, sourceRectangle, color);
+        }
+        
+
+
+    }
+}
